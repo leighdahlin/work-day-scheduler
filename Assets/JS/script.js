03044ed10b1d8151fce40fe350f6eed49a4f7d75
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-04-20 12:05:45 -0700

    found format for time block elements, working in js to compare time block to moment

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,8 +1,14 @@
 dateDisplayEl = $('#currentDay');
+NineAmEl = $('.NineAm');
+TimeEl = $('#time')
 
 function displayDate() {
     var rightNow = moment().format('dddd, MMMM Do');
     dateDisplayEl.text(rightNow);
+
+    var timeRightNow = moment().format('hh:mm')
+    console.log(timeRightNow);
+    TimeEl.text("09:00 am");
 };
 
 function init() {
@@ -10,3 +16,6 @@ function init() {
 };
 
 init();
+
+//Use below to populate input field when saved
+//NineAmEl.val("Hello");
